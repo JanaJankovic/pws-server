4123d901e3fcc2a80d36fa75042a21e583a66fed
Jana Jankovic <jana.jankovic@nomnio.com>
2023-10-13 17:07:19 +0200

    user model swagger

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,3 +1,75 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Notification:
+ *       type: object
+ *       required:
+ *         - id
+ *         - title
+ *         - type
+ *         - note
+ *         - date_time
+ *         - read
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the notification
+ *         title:
+ *           type: string
+ *           format: email
+ *           description: The title of notification
+ *         note:
+ *           type: string
+ *           description: Description of the notification
+ *         date_time:
+ *           type: string
+ *           format: date
+ *           description: Creation time of the notification
+ *         read:
+ *           type: boolean
+ *           description: Is notification read or unread
+ *       example:
+ *         id: 65294a27ec3717b1427b9358
+ *         title: Water tank empty!
+ *         type: warning
+ *         note: Water tank connected to recipient 65294a27ec3734b1427b4321 is empty.
+ *         date_time: 2020-03-20T14:28:23.382748
+ *         read: false
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the user
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: The email of your user
+ *         password:
+ *           type: string
+ *           description: The encrypted password of the user
+ *         notifications:
+ *           type: array
+ *           description: The notifications regarding watering system assigned to user
+ *           items:
+ *             $ref: '#/components/schemas/Notification'
+ *       example:
+ *         id: 65294a27ec3717b1427b9358
+ *         email: jana.j00@gmail.com
+ *         password: 123
+ *         notifications: []
+ */
+
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController.js");
